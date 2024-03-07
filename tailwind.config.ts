8881d7ceb851ea90017bd5f3a665d42a6bd952e3
Raftouch <rafanelkin@gmail.com>
2024-03-07 12:20:35 +0100

    init :tada: setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,24 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+    container: {
+      center: true,
+      padding: {
+        default: '1rem',
+        md: '1.5rem',
+        lg: '2rem',
       },
     },
+    extend: {
+      colors: {},
+    },
   },
-  plugins: [],
-};
-export default config;
+  plugins: [require('@tailwindcss/forms')],
+}
+export default config
