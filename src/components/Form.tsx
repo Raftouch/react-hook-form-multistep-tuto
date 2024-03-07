bff0921b8955b9fb4ea386afef9c2802229c589e
Raftouch <rafanelkin@gmail.com>
2024-03-07 12:52:04 +0100

    feat :bento: create zod schema

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,6 +1,9 @@
 'use client'
 
 import { useState } from 'react'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm, SubmitHandler } from 'react-hook-form'
 
 const steps = [
   {
